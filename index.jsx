6361f75490a022f18216e80723754c577d2be099
Brameshmadhav Srinivasan <sbmadhav@gmail.com>
2015-09-13 10:01:05 +0530

    Step 13 : React fetch data from ajax call and render in ui.

--- index.jsx
+++ index.jsx
@@ -1,24 +1,74 @@
 //below is just some comment -> it is not mandatory, it's just for our understanding.
 /**@jsx React.DOM */
 
-var Text = React.createClass ( {
+var GitHubProfile = React.createClass( {
+  getInitialState: function  () {
+    return {};
+  },
 
-  getDefaultProps : function  () {
-    return {
-      foregroundColor : 'red',
-      backgroundColor : 'green',
-    };
+  componentDidMount: function  () {
+    var self = this;
+    $.get( 'https://api.github.com/users/' + this.props.login , function  ( data ) {
+
+      self.setState( data );
+
+    } );
   },
 
   render: function() {
-    var styles = {
-      color : this.props.foregroundColor,
-      backgroundColor : this.props.backgroundColor 
-    };
     return (
-        <p style = {styles}> { this.props.content } </p>
+      <div>
+        <img src = { this.state.avatar_url } width = '80' />
+        <h3>{ this.state.name }</h3>   
+      </div>
     );
   }
+
+});
+
+var Form = React.createClass( {
+
+  onSubmit : function  ( e ) {
+    e.preventDefault();
+    var loginInput = React.findDOMNode( this.refs.login );
+    this.props.addGitHubProfile( loginInput.value );
+    loginInput.value = '';
+  },
+
+  render: function() {
+    return (
+       <form onSubmit = { this.onSubmit }>
+        <input ref='login' placeholder= 'Please enter your Github login' />
+        <button>Submit</button>
+      </form>
+    );
+  }
+
 } );
 
-React.render( <Text content ="React js" /> , document.getElementById("target") );
+var Main = React.createClass( {
+
+  getInitialState: function  () {
+    return { logins: [] };
+  },
+
+  addGitHubProfile : function  ( login ) {
+    this.setState( { logins: this.state.logins.concat( login ) } );
+  },
+
+  render: function() {
+    var githubprofiles = this.state.logins.map( function  ( login ) {
+      return ( <GitHubProfile login = { login } /> );
+    });
+    return (
+      <div>
+        <Form addGitHubProfile = { this.addGitHubProfile } />
+        <hr />
+        { githubprofiles }
+      </div>
+    );
+  }
+
+} );
+
+React.render( <Main /> , document.getElementById( 'target' ) );
