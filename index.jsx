922b86c524a0ffde43b5aa4979b1e040e4cd651e
Brameshmadhav Srinivasan <sbmadhav@gmail.com>
2015-09-13 10:56:05 +0530

    Step 14 : Two way data binding using LinkedStateMixin

--- index.jsx
+++ index.jsx
@@ -1,74 +1,31 @@
 //below is just some comment -> it is not mandatory, it's just for our understanding.
 /**@jsx React.DOM */
 
-var GitHubProfile = React.createClass( {
-  getInitialState: function() {
-    return {};
+var ContainerComponent = React.createClass( {
+  mixins: [ React.addons.LinkedStateMixin ],
+  getInitialState: function  () {
+    return {
+      name : ''
+    }
   },
-
-  componentDidMount: function() {
-    var self = this;
-    $.get( 'https://api.github.com/users/' + this.props.login , function( data ) {
-
-      self.setState( data );
-
-    } );
-  },
-
   render: function() {
     return (
-      <div>
-        <img src = { this.state.avatar_url } width = '80' />
-        <h3>{ this.state.name }</h3>   
+      <div className = '' >
+      <InputBox setValue = { this.linkState( 'name' ) } />
+        <h4>{ this.state.name }</h4>
       </div>
+    );
+  }
+} );
+
+
+var InputBox = React.createClass( {
+  render: function() {
+    return (
+      <input type = 'text' valueLink = { this.props.setValue } placeholder = 'Type some text'/>
     );
   }
 
 });
 
-var Form = React.createClass( {
-
-  onSubmit : function( e ) {
-    e.preventDefault();
-    var loginInput = React.findDOMNode( this.refs.login );
-    this.props.addGitHubProfile( loginInput.value );
-    loginInput.value = '';
-  },
-
-  render: function() {
-    return (
-       <form onSubmit = { this.onSubmit }>
-        <input ref='login' placeholder= 'Please enter your Github login' />
-        <button>Submit</button>
-      </form>
-    );
-  }
-
-} );
-
-var Main = React.createClass( {
-
-  getInitialState: function() {
-    return { logins: [] };
-  },
-
-  addGitHubProfile : function( login ) {
-    this.setState( { logins: this.state.logins.concat( login ) } );
-  },
-
-  render: function() {
-    var githubprofiles = this.state.logins.map( function( login ) {
-      return ( <GitHubProfile login = { login } /> );
-    });
-    return (
-      <div>
-        <Form addGitHubProfile = { this.addGitHubProfile } />
-        <hr />
-        { githubprofiles }
-      </div>
-    );
-  }
-
-} );
-
-React.render( <Main /> , document.getElementById( 'target' ) );
+React.render( <ContainerComponent /> , document.getElementById( 'target' ) );
