be5130897506a670e329f26260c411eb92090ec5
Brameshmadhav Srinivasan <sbmadhav@gmail.com>
2015-09-13 12:06:29 +0530

    Step 16 : Controlled components - Input data

--- index.jsx
+++ index.jsx
@@ -1,31 +1,19 @@
 //below is just some comment -> it is not mandatory, it's just for our understanding.
 /**@jsx React.DOM */
-
-var ContainerComponent = React.createClass( {
-  mixins: [ React.addons.LinkedStateMixin ],
+var FormComponent = React.createClass( {
   getInitialState: function  () {
-    return {
-      name : ''
-    }
+    return { init: '' };
+  },
+  handleChangeEvent: function( e ){
+    this.setState( { init : e.target.value } );
+    // console.log( e.target.value );
   },
   render: function() {
     return (
-      <div className = '' >
-      <InputBox setValue = { this.linkState( 'name' ) } />
-      <h4>{ this.state.name }</h4>
-      </div>
-    );
-  }
-} );
-
-
-var InputBox = React.createClass( {
-  render: function() {
-    return (
-      <input type = 'text' valueLink = { this.props.setValue } placeholder = 'Type some text'/>
+      <input type = 'text' onChange = { this.handleChangeEvent } value = { this.state.init } placeholder = 'Type some text'/>
     );
   }
 
 });
 
-React.render( <ContainerComponent /> , document.getElementById( 'target' ) );
+React.render( <FormComponent /> , document.getElementById( 'target' ) );
